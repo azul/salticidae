b32f57d46034f5289d5fc26ce266d36c5f4132f5
Azul <azul@leap.se>
2012-07-19 16:07:02 +0200

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Salticidae::Application.config.secret_token = 'e70f9c1d6407908db046f4fd6747c54cfdd649a87d71089433833b12f323207ae0c7b358c53b890c8030fec73704a8d5ef7a84ac39c6605c31ea550bf10bc130'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Salticidae::Application.routes.draw do
+  root :to => "home#index"
+end
