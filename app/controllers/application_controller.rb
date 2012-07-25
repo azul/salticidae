e9c3978ea63465248b058e477c1c8f9991f6e5d5
Azul <azul@leap.se>
2012-07-25 17:12:56 +0200

    it's an unauthenticated user not an unauthorized one.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) || UnauthorizedUser.new
+    @current_user ||= User.find(session[:user_id]) || UnauthenticatedUser.new
   end
 
   def logged_in?
-    !current_user.is_a? UnauthorizedUser
+    !current_user.is_a? UnauthenticatedUser
   end
 end
