0f2323accb99e742c18017ac2ac8242a72419cfd
Azul <azul@leap.se>
2012-08-20 18:49:54 +0200

    implemented picking a unique random cert and destroying it

--- app/models/cert.rb
+++ app/models/cert.rb
@@ -1,2 +1,32 @@
 class Cert < CouchRest::Model::Base
+  timestamps!
+
+  property :random, Float, :accessible => false
+
+  before_create :set_random
+
+  design do
+    view :by_random
+  end
+
+  class << self
+    def sample
+      self.by_random.startkey(rand).first || self.by_random.first
+    end
+
+    def pick_from_pool
+      cert = self.sample
+      cert.destroy
+      return cert
+    rescue RESOURCE_NOT_FOUND
+      retry if Cert.count > 0
+      raise RECORD_NOT_FOUND
+    end
+  end
+
+  def set_random
+    self.random = rand
+  end
+
+
 end
--- config/initializers/validations.rb
+++ config/initializers/validations.rb
@@ -1,4 +1,5 @@
 # In case we use a different ORM at some point
 VALIDATION_FAILED = CouchRest::Model::Errors::Validations
 RECORD_NOT_FOUND  = CouchRest::Model::DocumentNotFound
+RESOURCE_NOT_FOUND  = RestClient::ResourceNotFound
 WRONG_PASSWORD = SRP::WrongPassword
